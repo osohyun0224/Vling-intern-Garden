0a287efcc603215fdf595041ae95023e52d58e37
osohyun0224 <osohyun0224@naver.com>
2024-02-13 11:42:23 +0900

    🐛  fix: 모노레포 정적 파일 개발로 인한 이미지 이슈 수정

--- packages/shared-ui/src/components/Header/Header.jsx
+++ packages/shared-ui/src/components/Header/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import Logo from '../../../../public/images/svg/vling_logo.svg';
-import Image from 'next/image'; // Image 컴포넌트를 불러옵니다.
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        {/* span 대신 Image 컴포넌트를 사용하여 로고 이미지를 표시합니다. */}
-        <Image src={Logo} alt="Logo" width={60} height={60} />
+        <img src="/images/svg/vling_new_logo.svg" alt="Logo" style={{ width: '60px', height: '60px' }} />
       </div>
     </div>
   );
