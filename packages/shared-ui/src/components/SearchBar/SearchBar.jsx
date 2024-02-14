d6b38791837398699b0b16687a84286a68c87f6c
osohyun0224 <osohyun0224@naver.com>
2024-02-14 12:21:09 +0900

    🐛 fix: 검색바 컴포넌트 버튼  버그 수정

--- packages/shared-ui/src/components/SearchBar/SearchBar.jsx
+++ packages/shared-ui/src/components/SearchBar/SearchBar.jsx
@@ -4,13 +4,16 @@ import SearchIcon from '../../../../../workspace_template/exportStaticExample/pu
 
 const SearchBar = () => {
   return (
-    <div className={styles.searchContainer}>
-      <SearchIcon className={styles.searchIcon}/>
-      <input
-        className={styles.searchInput}
-        type="text"
-        placeholder="관심있는 분야 / 회사를 입력하세요."
-      />
+    <div className={styles.wrapper}>
+      <div className={styles.searchContainer}>
+        <SearchIcon className={styles.searchIcon}/>
+        <input
+          className={styles.searchInput}
+          type="text"
+          placeholder="관심있는 분야 / 회사를 입력하세요."
+        />
+      </div>
+      <button className={styles.searchButton}>검색</button> {/* 검색 버튼을 searchContainer 바깥으로 이동 */}
     </div>
   );
 };
