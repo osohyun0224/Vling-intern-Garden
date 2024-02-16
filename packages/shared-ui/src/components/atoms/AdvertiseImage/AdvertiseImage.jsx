418bcc8419c8f7e0b89c8acdb43ac2ffa6d62784
osohyun0224 <osohyun0224@naver.com>
2024-02-16 15:35:49 +0900

    ♻️  refactor: 진행 중인 광고 이미지 아토믹 패턴 적용하여 코드 리팩토링

---
+++ packages/shared-ui/src/components/atoms/AdvertiseImage/AdvertiseImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './AdvertiseImage.module.scss';
+
+const AdvertiseImage = ({ src, alt, liked, toggleLike }) => {
+  return (
+    <div className={styles.container}>
+      <img src={src} alt={alt} className={styles.adImage} />
+      <img src={liked ? "/images/png/Love.png" : "/images/png/Favorite.png"} alt="Like" className={styles.likeIcon} onClick={toggleLike} />
+    </div>
+  );
+};
+
+export default AdvertiseImage;
