10f6ffb48f6593db613bcd9d1d398dc4d3663b86
osohyun0224 <osohyun0224@naver.com>
2024-02-16 11:14:42 +0900

    ✨ feat: tossface 프로젝트 세팅 및 직접 적용

--- workspace_template/exportStaticExample/src/pages/_app.jsx
+++ workspace_template/exportStaticExample/src/pages/_app.jsx
@@ -2,5 +2,12 @@ import '../styles/globals.css'
 import '../../public/static/font/style.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin />
+        <link href="https://cdn.jsdelivr.net/gh/toss/tossface/dist/tossface.css" rel="stylesheet" />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- workspace_template/exportStaticExample/src/pages/example.jsx
+++ workspace_template/exportStaticExample/src/pages/example.jsx
@@ -3,6 +3,7 @@ import Title from '@bzznbyd/atoms-title-text';
 import SubTitle from '@bzznbyd/atoms-subtitle-text';
 import IntroText from '@bzznbyd/atoms-intro-text';
 import SearchBar from '@bzznbyd/temp-searchbar';
+import styles from'./example.module.scss';
 
 export default function Example() {
   return (
@@ -11,7 +12,7 @@ export default function Example() {
       <Title>광고주 목록</Title>
       <IntroText />
       <SearchBar />
-      <SubTitle>광고주가 찾고 있어요!👀</SubTitle>
+      <SubTitle>광고주가 찾고 있어요!<span className={styles.tossface}> 👀</span></SubTitle>
     </div>
   );
 }
