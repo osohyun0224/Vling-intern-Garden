33736276d10c36b1496fae7fc3167127d1fc18d3
osohyun0224 <osohyun0224@naver.com>
2024-02-16 16:11:41 +0900

    ♻️  refactor: 광고주 프로필 이미지 아토믹 패턴 적용 리팩토링

---
+++ packages/shared-ui/src/components/atoms/AdProfileImage/AdProfileImage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './AdProfileImage.module.scss;'
+
+const AdProfileImage = ({ src, alt }) => {
+  return <img src={src} alt={alt} className={styles.authorImage} />;
+};
+
+export default AdProfileImage;
