f4be08539831f4c9e7c1c3705b321a516daca5ef
osohyun0224 <osohyun0224@naver.com>
2024-02-18 16:13:07 +0900

    ✨ feat: 좋아요 기능에 트렌젝션 추가하여 애니메이션 효과 추가

--- packages/shared-ui/src/components/atoms/AdvertiseImage/AdvertiseImage.jsx
+++ packages/shared-ui/src/components/atoms/AdvertiseImage/AdvertiseImage.jsx
@@ -5,7 +5,12 @@ const AdvertiseImage = ({ src, alt, liked, toggleLike }) => {
   return (
     <div className={styles.container}>
       <img src={src} alt={alt} className={styles.adImage} />
-      <img src={liked ? "/images/png/Love.png" : "/images/png/Favorite.png"} alt="Like" className={styles.likeIcon} onClick={toggleLike} />
+      <img 
+        src={liked ? "/images/png/Love.png" : "/images/png/Favorite.png"} 
+        alt="Like" 
+        className={`${styles.likeIcon} ${liked ? styles.liked : ''}`}
+        onClick={toggleLike} 
+      />
     </div>
   );
 };
