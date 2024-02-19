0a066205c08ab14f7b37844318865a63dc96c983
osohyun0224 <osohyun0224@naver.com>
2024-02-19 12:08:00 +0900

    🐛  fix: 광고주 목록 컴포넌트의 확대축소에 따라 위치가 변경되는 버그 수정

--- workspace_template/exportStaticExample/src/pages/example.jsx
+++ workspace_template/exportStaticExample/src/pages/example.jsx
@@ -1,24 +1,28 @@
-import Header  from '@bzznbyd/temp-header';
-import Title from '@bzznbyd/atoms-title-text';
-import SubTitle from '@bzznbyd/atoms-subtitle-text';
-import IntroText from '@bzznbyd/atoms-intro-text';
-import SearchBar from '@bzznbyd/temp-searchbar';
-import NowAdvertise from '@bzznbyd/temp-now-advertise';
-import Advertiselist from '@bzznbyd/temp-advertiser-list';
-import styles from'./example.module.scss';
+import Header from '@bzznbyd/temp-header'
+import Title from '@bzznbyd/atoms-title-text'
+import SubTitle from '@bzznbyd/atoms-subtitle-text'
+import IntroText from '@bzznbyd/atoms-intro-text'
+import SearchBar from '@bzznbyd/temp-searchbar'
+import NowAdvertise from '@bzznbyd/temp-now-advertise'
+import Advertiselist from '@bzznbyd/temp-advertiser-list'
+import styles from './example.module.scss'
 
 export default function Example() {
   return (
     <div>
       <Header />
-      <Title>광고주 목록</Title>
-      <IntroText />
-      <SearchBar />
-      <SubTitle>광고주가 찾고 있어요!<span className={styles.tossface}> 👀</span></SubTitle>
-      <NowAdvertise/>
-      <Advertiselist/>
+      <div className={styles.container}>
+        <Title>광고주 목록</Title>
+        <IntroText />
+        <SearchBar />
+        <SubTitle>
+          광고주가 찾고 있어요!<span className={styles.tossface}> 👀</span>
+        </SubTitle>
+        <NowAdvertise />
+        <Advertiselist />
+      </div>
     </div>
-  );
+  )
 }
 
 export async function getStaticProps({ req, res, locale, query }) {
