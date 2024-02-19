0a56fa8093818fad9451707d784180650d5dfe5d
osohyun0224 <osohyun0224@naver.com>
2024-02-19 16:02:45 +0900

    ✨ feat: PDF 저장해 다운로드 받는 기능 구현

--- packages/shared-ui/src/components/ReportModal/ReportModal.jsx
+++ packages/shared-ui/src/components/ReportModal/ReportModal.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react'
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
 import styles from './ReportModal.module.scss'
 import ChannelHeader from '@bzznbyd/temp-channel-header'
 
@@ -12,21 +14,52 @@ const ReportModal = ({ show, onClose }) => {
     }
   }, [show])
 
+  const savePdf = () => {
+    const modalContent = document.getElementById('modalContent')
+    html2canvas(modalContent, {
+      windowHeight: modalContent.scrollHeight,
+      windowWidth: modalContent.scrollWidth
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png')
+      const imgWidth = 210
+      const pageHeight = 295
+      const imgHeight = (canvas.height * imgWidth) / canvas.width
+      let heightLeft = imgHeight
+
+      const pdf = new jsPDF('p', 'mm')
+      let position = 0
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+      heightLeft -= pageHeight
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight
+        pdf.addPage()
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+        heightLeft -= pageHeight
+      }
+
+      pdf.save('report.pdf')
+    })
+  }
+
   if (!show) {
     return null
   }
 
   return (
     <div className={styles.modalBackdrop} onClick={onClose}>
-      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()} id="modalContent">
         <div className={styles.header}>
           <div className={styles.title}>광고 제안서</div>
           <img src={'/images/png/Close.png'} alt="Close" className={styles.closeButton} onClick={onClose} />
         </div>
         <hr className={styles.divider} />
         <div className={styles.reporttitle}>채널 정보</div>
-        <ChannelHeader/>
-        <button className={styles.button}>pdf로 저장하기</button>
+        <ChannelHeader />
+        <button className={styles.button} onClick={savePdf}>
+          pdf로 저장하기
+        </button>
       </div>
     </div>
   )
