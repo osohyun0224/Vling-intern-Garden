45553fb5b47c7e223d790d5e300a2772233b1a6a
osohyun0224 <osohyun0224@naver.com>
2024-02-19 14:20:42 +0900

    💬  add: 스타일 확인을 위한 임시 데이터 텍스트 추가

--- packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
+++ packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from './ChannelHeader.module.scss';
 
-const ChannelHeader = ({ channelImage, channelName, channelCategory }) => {
+const ChannelHeader = () => {
+  const channelImage = "https://yt3.googleusercontent.com/ytc/AIf8zZRsiaDKzz7z9MLYs3wnZ1o7-L-W9SPG9X6RQ0XM=s176-c-k-c0x00ffffff-no-rj";
+  const channelName = "판교 뚜벅쵸";
+  const channelCategories = ["Vlog/일상", "지식/정보", "IT/과학기술"];
+
   return (
     <div className={styles.channelHeader}>
       <img src={channelImage} alt="Channel" className={styles.channelImage} />
       <div className={styles.channelInfo}>
         <div className={styles.channelName}>{channelName}</div>
-        <div className={styles.channelCategory}>{channelCategory}</div>
+        <div className={styles.channelCategories}>
+          {channelCategories.map((category, index) => (
+            <div key={index} className={`${styles.channelCategory} ${index === channelCategories.length - 1 ? styles.lastCategory : ''}`}>
+              {category}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
