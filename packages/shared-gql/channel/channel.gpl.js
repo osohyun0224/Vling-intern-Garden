7e0ee88610194e92bdfcb495163fbef8b64a6bce
osohyun0224 <osohyun0224@naver.com>
2024-02-20 18:29:24 +0900

    ✨ feat: 채널 레포트 채널 헤더의 모든 정보 쿼리 데이터 연결

--- packages/shared-gql/channel/channel.gpl.js
+++ packages/shared-gql/channel/channel.gpl.js
@@ -1,14 +1,21 @@
 import { gql } from '@apollo/client'
 
-export const getChannel=gql`
-query getChannel($channelId: String!) {
+export const getChannel = gql`
+  query getChannel($channelId: String!) {
     channel(channelId: $channelId) {
       ##{채널의 헤더에서 사용하는 정보}
       # 채널 제목
       title
       # 채널의 이미지
       thumbnails
-      #
+      # 알고리즘 스코어
+      algosinLikesPerVideo
+      # 구독자 수
+      subscriberCount
+      # 활성도 
+      activePercentLevel
+      # 참여도
+      reactRatioLevel
     }
   }
 `
--- packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
+++ packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
@@ -3,6 +3,15 @@ import { useQuery } from '@apollo/client';
 import styles from './ChannelHeader.module.scss';
 import { getChannel } from '../../../../shared-gql/channel/channel.gpl';
 
+// 시청자 참여도 및 구독자 활성도를 위한 매핑
+const levelMapping = {
+  1: '매우 높음',
+  2: '높음',
+  3: '보통',
+  4: '낮음',
+  5: '매우 낮음',
+};
+
 const ChannelHeader = () => {
   const channelId = "UCZ3dxObRPEJzoryEyQqmhWg"; 
 
@@ -16,8 +25,21 @@ const ChannelHeader = () => {
     return <p>Error: {error.message}</p>; 
   }
 
-  const { title: channelName, thumbnails: channelImage } = data.channel;
-  const channelCategories = ['Vlog/일상', '지식/정보', 'IT/과학기술']
+  const {
+    title: channelName,
+    thumbnails: channelImage,
+    algosinLikesPerVideo,
+    subscriberCount,
+    activePercentLevel,
+    reactRatioLevel,
+  } = data.channel;
+
+  const subscriberCountFormatted = `${Math.floor(subscriberCount / 10000)}만`;
+  const algosinLikesPerVideoFormatted = `${Math.floor(algosinLikesPerVideo)}` + (algosinLikesPerVideo >= 50 ? ' 높음' : '');
+  const reactRatioLevelText = levelMapping[reactRatioLevel];
+  const activePercentLevelText = levelMapping[activePercentLevel];
+
+  const channelCategories = ['Vlog/일상', '지식/정보', 'IT/과학기술'];
 
   return (
     <div className={styles.channelHeader}>
@@ -26,10 +48,7 @@ const ChannelHeader = () => {
         <div className={styles.channelName}>{channelName}</div>
         <div className={styles.channelCategories}>
           {channelCategories.map((category, index) => (
-            <div
-              key={index}
-              className={`${styles.channelCategory} ${index === channelCategories.length - 1 ? styles.lastCategory : ''}`}
-            >
+            <div key={index} className={`${styles.channelCategory} ${index === channelCategories.length - 1 ? styles.lastCategory : ''}`}>
               {category}
             </div>
           ))}
@@ -39,24 +58,24 @@ const ChannelHeader = () => {
         <div className={styles.subscribers}>
           <img src="/images/png/People.png" alt="Subscribers" className={styles.statIcon} />
           <span className={styles.statTitle}>채널의 구독자 수 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <span className={styles.statValue}>4만</span>
+          <span className={styles.statValue}>{subscriberCountFormatted}</span>
         </div>
         <div className={styles.algorithmScore}>
           <img src="/images/png/Ratings.png" alt="Algorithm Score" className={styles.statIcon} />
           <span className={styles.statTitle}>알고리즘 스코어</span>
-          <span className={styles.statValue}>58 높음</span>
+          <span className={styles.statValue}>{algosinLikesPerVideoFormatted}</span>
         </div>
       </div>
       <div className={styles.rightStats}>
         <div className={styles.viewerEngagement}>
           <img src="/images/png/Volunteering.png" alt="Viewer Engagement" className={styles.statIcon} />
           <span className={styles.statTitle}>시청자 참여도</span>
-          <span className={styles.statValue}>높음</span>
+          <span className={styles.statValue}>{reactRatioLevelText}</span>
         </div>
         <div className={styles.subscriberActivity}>
           <img src="/images/png/Radioactive.png" alt="Subscriber Activity" className={styles.statIcon} />
           <span className={styles.statTitle}>구독자 활성도</span>
-          <span className={styles.statValue}>낮음</span>
+          <span className={styles.statValue}>{activePercentLevelText}</span>
         </div>
       </div>
       <div className={styles.buttonGroup}>
@@ -66,7 +85,7 @@ const ChannelHeader = () => {
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChannelHeader
+export default ChannelHeader;
