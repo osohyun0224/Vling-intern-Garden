0790a7172cb66ada18051967318823a2662fabe5
osohyun0224 <osohyun0224@naver.com>
2024-02-20 09:46:15 +0900

    ✨ feat: 해당 채널의 구독자, 알고신, 시청자 참여, 구독자 활성도 메인 정보 구현 추가

--- packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
+++ packages/shared-ui/src/components/ChannelHeader/ChannelHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './ChannelHeader.module.scss';
 
 const ChannelHeader = () => {
-  const channelImage = "https://yt3.googleusercontent.com/ytc/AIf8zZRsiaDKzz7z9MLYs3wnZ1o7-L-W9SPG9X6RQ0XM=s176-c-k-c0x00ffffff-no-rj";
-  const channelName = "판교 뚜벅쵸";
-  const channelCategories = ["Vlog/일상", "지식/정보", "IT/과학기술"];
+  const channelImage = 'https://yt3.googleusercontent.com/ytc/AIf8zZRsiaDKzz7z9MLYs3wnZ1o7-L-W9SPG9X6RQ0XM=s176-c-k-c0x00ffffff-no-rj';
+  const channelName = '판교 뚜벅쵸';
+  const channelCategories = ['Vlog/일상', '지식/정보', 'IT/과학기술'];
 
   return (
     <div className={styles.channelHeader}>
@@ -17,6 +17,30 @@ const ChannelHeader = () => {
               {category}
             </div>
           ))}
+        </div>
+      </div>
+      <div className={styles.leftStats}>
+        <div className={styles.subscribers}>
+          <img src="/images/png/People.png" alt="Subscribers" className={styles.statIcon} />
+          <span className={styles.statTitle}>구독자 수</span>
+          <span className={styles.statValue}>4만</span>
+        </div>
+        <div className={styles.algorithmScore}>
+          <img src="/images/png/Ratings.png" alt="Algorithm Score" className={styles.statIcon} />
+          <span className={styles.statTitle}>알고리즘 스코어</span>
+          <span className={styles.statValue}>58 높음</span>
+        </div>
+      </div>
+      <div className={styles.rightStats}>
+        <div className={styles.viewerEngagement}>
+          <img src="/images/png/Volunteering.png" alt="Viewer Engagement" className={styles.statIcon} />
+          <span className={styles.statTitle}>시청자 참여도</span>
+          <span className={styles.statValue}>높음</span>
+        </div>
+        <div className={styles.subscriberActivity}>
+          <img src="/images/png/Radioactive.png" alt="Subscriber Activity" className={styles.statIcon} />
+          <span className={styles.statTitle}>구독자 활성도</span>
+          <span className={styles.statValue}>낮음</span>
         </div>
       </div>
     </div>
